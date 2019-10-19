a26d09bc086658be9d9580148c0d3d90f8e251d8
ncl7 <ncl7@njit.edu>
2019-10-18 21:47:09 -0400

    Fix: attempted to fix errors

--- src/NLcalculatorTests.py
+++ src/NLcalculatorTests.py
@@ -4,32 +4,30 @@ from NLcalculator import NLCalculator
 
 class MyTestCase(unittest.TestCase):
 
-    calculator: NLCalculator
-
-    def setup(self) -> None:
+    def setUp(self) -> None:
         self.calculator = NLCalculator()
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, NLCalculator)
 
     def test_results_property_calculator(self):
-        self.assertEqual(NLCalculator.result, 2)
+        self.assertEqual(self.calculator.result, 2)
 
     def test_add_method_calculator(self):
-        self.assertEqual(NLCalculator.add(1, 1), 2)
-        self.assertEqual(NLCalculator.result, 2)
+        self.assertEqual(self.calculator.add(1, 1), 2)
+        self.assertEqual(self.calculator.result, 2)
 
     def test_subtract_method_calculator(self):
-        self.assertEqual(NLCalculator.subtract(2, 1), 1)
-        self.assertEqual(NLCalculator.result, 1)
+        self.assertEqual(self.calculator.subtract(2, 1), 1)
+        self.assertEqual(self.calculator.result, 1)
 
     def test_division_method_calculator(self):
-        self.assertEqual(NLCalculator.division(2, 1), 2)
-        self.assertEqual(NLCalculator.result, 2)
+        self.assertEqual(self.calculator.division(2, 1), 2)
+        self.assertEqual(self.calculator.result, 2)
 
     def test_multiply_method_calculator(self):
-        self.assertEqual(NLCalculator.multiply(3, 1), 3)
-        self.assertEqual(NLCalculator.result, 3)
+        self.assertEqual(self.calculator.multiply(3, 1), 3)
+        self.assertEqual(self.calculator.result, 3)
 
 
 if __name__ == '__main__':
