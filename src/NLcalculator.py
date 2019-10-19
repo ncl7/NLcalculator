4d44d3869448289545cedda7afdb2e167486788c
ncl7 <ncl7@njit.edu>
2019-10-18 20:05:07 -0400

    Feature: added test for result

--- src/NLcalculator.py
+++ src/NLcalculator.py
@@ -1,5 +1,8 @@
 
 class NLCalculator:
+    result = 0
 
     def __init__(self):
+        x = 2 + 2
+        self.result = x;
         pass
--- src/NLcalculatorTests.py
+++ src/NLcalculatorTests.py
@@ -8,6 +8,10 @@ class MyTestCase(unittest.TestCase):
         calculator = NLCalculator()
         self.assertIsInstance(calculator, NLCalculator)
 
+    def test_results_property_calculator(self):
+        calculator = NLCalculator()
+        self.assertEquals(calculator.result, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
