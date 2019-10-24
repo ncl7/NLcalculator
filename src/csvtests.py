7071d646519d9c5c19244e8c066719be4b7b18b2
ncl7 <ncl7@njit.edu>
2019-10-24 19:56:56 -0400

    Feature: added code for csv tests

--- src/csvtests.py
+++ src/csvtests.py
@@ -1,9 +1,14 @@
 import unittest
+from CsvReader import CsvReader
+from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def setUp(self) -> None:
+        self.csv_reader = CsvReader('employee_birthday.txt')
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.csv_reader, CsvReader)
 
 
 if __name__ == '__main__':
