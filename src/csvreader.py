4b2f50c98d7d4e775f3f1c3108a7765beb7cd70c
ncl7 <ncl7@njit.edu>
2019-10-24 19:36:49 -0400

    Feature: added code for csv reader

---
+++ src/csvreader.py
@@ -0,0 +1,13 @@
+import csv
+from pprint import pprint
+
+
+class CsvReader:
+
+    def __init__(self, filepath):
+        with open(filepath) as text_data:
+            csv_data = csv.DictReader(text_data, delimiter=",")
+            for row in csv_data:
+                self.data.append(row)
+        pprint(self.data)
+        pass
