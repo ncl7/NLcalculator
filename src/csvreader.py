bc3dd0c0e28ac24368fdc06f58b68419c86b5718
ncl7 <ncl7@njit.edu>
2019-10-27 11:11:50 -0400

    Fix: moved empty data list inside function

--- src/csvreader.py
+++ src/csvreader.py
@@ -3,14 +3,14 @@ from pprint import pprint
 
 
 class CsvReader:
-    data = []
 
     def __init__(self, filepath):
+        data = []
         with open(filepath) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=",")
             for row in csv_data:
-                self.data.append(row)
-        pprint(self.data)
+                data.append(row)
+        pprint(data)
 
     def return_data_as_objects(self, class_name):
         objects = []
