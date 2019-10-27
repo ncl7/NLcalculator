e4f77d7b691c5b4af9bee9cbe9b4448e253ac434
ncl7 <ncl7@njit.edu>
2019-10-27 18:13:56 -0400

    Fix: removed unnecessary code from csvtests file

--- src/csvtests.py
+++ src/csvtests.py
@@ -11,13 +11,6 @@ class MyTestCase(unittest.TestCase):
         self.csv_reader = CsvReader('Unit Test Square.csv')
         self.csv_reader = CsvReader('Unit Test Square Root.csv')
 
-    def test_return_data_as_objects(self):
-        people = self.csv_reader.return_data_as_objects('person')
-        self.assertIsInstance(people, list)
-        test_class = type('person', self.csv_reader.data[0])
-        for person in people:
-            self.assertEqual(person.__name__, test_class.__name__)
-
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.csv_reader, CsvReader)
 
