9f546ba6701de43a65b1055e4ec8fdbe06c06b7f
ncl7 <ncl7@njit.edu>
2019-10-27 17:19:58 -0400

    Feature: added unit test square root to csvtests file

--- src/csvtests.py
+++ src/csvtests.py
@@ -10,6 +10,7 @@ class MyTestCase(unittest.TestCase):
         self.csv_reader = CsvReader('Unit Test Division.csv')
         self.csv_reader = CsvReader('Unit Test Multiplication.csv')
         self.csv_reader = CsvReader('Unit Test Square.csv')
+        self.csv_reader = CsvReader('Unit Test Square Root.csv')
 
     def test_return_data_as_objects(self):
         people = self.csv_reader.return_data_as_objects('person')
