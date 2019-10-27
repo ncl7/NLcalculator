09f4cb1881f882e704d45bc4ef4b287d12dda42a
ncl7 <ncl7@njit.edu>
2019-10-27 18:05:51 -0400

    Fix: removed unnecessary code from NLcalculatorTests

--- src/NLcalculatorTests.py
+++ src/NLcalculatorTests.py
@@ -1,7 +1,6 @@
 import unittest
 from NLcalculator import NLCalculator
 from csvreader import CsvReader
-from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
@@ -36,26 +35,17 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-        self.assertEqual(self.calculator.multiply(3, 1), 3)
-        self.assertEqual(self.calculator.result, 3)
-
     def test_square_method_calculator(self):
         test_data = CsvReader('/src/Unit Test Square.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.square(row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
-        self.assertEqual(self.calculator.square(4), 16)
-        self.assertEqual(self.calculator.result, 16)
-
     def test_sqrt_method_calculator(self):
         test_data = CsvReader('/src/Unit Test Square Root.csv').data
         for row in test_data:
             self.assertEqual(self.calculator.sqrt(row['Value 1']), float(row['Result']))
             self.assertEqual(self.calculator.result, float(row['Result']))
-
-        self.assertEqual(self.calculator.sqrt(25), 5)
-        self.assertEqual(self.calculator.result, 5)
 
 
 if __name__ == '__main__':
